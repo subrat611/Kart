b40a313ebc87b43706515dee719185b1d829757e
subrat <subratj611@gmail.com>
2023-01-07 10:37:00 +0530

    header cart icon update

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { Button } from "@mui/material";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 import styles from "./header.module.scss";
 
@@ -14,49 +15,7 @@ export default function Header() {
           Login
         </Button>
         <Button variant="text">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512"
-            className={`${styles.header_cartbtn} ionicon`}
-          >
-            <title>Cart</title>
-            <circle
-              cx="176"
-              cy="416"
-              r="16"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="32"
-            />
-            <circle
-              cx="400"
-              cy="416"
-              r="16"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="32"
-            />
-            <path
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="32"
-              d="M48 80h64l48 272h256"
-            />
-            <path
-              d="M160 288h249.44a8 8 0 007.85-6.43l28.8-144a8 8 0 00-7.85-9.57H128"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="32"
-            />
-          </svg>
+          <ShoppingCartIcon className={`${styles.header_cartbtn} ionicon`} />
         </Button>
       </div>
     </div>
